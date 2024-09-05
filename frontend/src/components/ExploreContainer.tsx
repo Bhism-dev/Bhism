d09aafa68c7a48c179c7ed7816c9e7950172c6a0
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-09-06 00:02:28 +0530

    Deleted TailWindExample

--- frontend/src/components/ExploreContainer.tsx
+++ frontend/src/components/ExploreContainer.tsx
@@ -7,18 +7,10 @@ interface ContainerProps {
 
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   const History = useHistory();
-  const handleClick = () => {
-    History.push('/tailwind-example');
-  }
   return (
     <div id="container">
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components" className='text-blue-500'>UI Components</a></p>
-      <button
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer m-8 transition duration-300 transform hover:scale-105'
-        onClick={handleClick}>
-        Tailwind-Example
-      </button>
     </div>
   );
 };
