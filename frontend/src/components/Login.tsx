6bbc3ac4fc2cff2a2b16598a1cc633cd12beec89
hershit-shukla <hershitshukla35@gmail.com>
2024-09-07 22:38:23 +0530

    chore: Update npm dependencies and add new packages for frontend

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,19 +1,34 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { IonContent, IonPage, IonSegment, IonSegmentButton, IonLabel, IonInput, IonButton, IonItem, IonText, IonAlert, IonInputPasswordToggle, } from '@ionic/react';
-import '../theme/tailwind.css'; // Import the Tailwind CSS file
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import {
+  IonContent,
+  IonPage,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonInput,
+  IonButton,
+  IonItem,
+  IonText,
+  IonAlert,
+  IonInputPasswordToggle,
+} from "@ionic/react";
+import { PinInput } from "react-input-pin-code";
+import { useMaskito } from "@maskito/react";
+import "../theme/tailwind.css"; // Import the Tailwind CSS file
 
 const LoginForm: React.FC = () => {
-  const [segment, setSegment] = useState<'mobile' | 'abha'>('mobile');
+  const [segment, setSegment] = useState<"mobile" | "abha">("mobile");
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [showOtp, setShowOtp] = useState<boolean>(false);
   const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
-  const [otp, setOtp] = useState<string[]>(['', '', '', '', '', '']);
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
   const inputRefs = useRef<(HTMLIonInputElement | null)[]>(Array(6).fill(null));
   const [submit, setSubmit] = useState(false);
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
-  const otpRefs = useRef<(HTMLIonInputElement | null)[]>(Array(6).fill(null));
+
+  // const [otp, setValues] = React.useState(['', '', '']);
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -32,7 +47,7 @@ const LoginForm: React.FC = () => {
   }, [submit, phone, password]);
 
   const handleSegmentChange = (e: CustomEvent) => {
-    setSegment(e.detail.value as 'mobile' | 'abha');
+    setSegment(e.detail.value as "mobile" | "abha");
     setShowPassword(false);
     setShowOtp(false);
   };
@@ -42,71 +57,62 @@ const LoginForm: React.FC = () => {
   };
 
   const handleLoginWithOtpClick = async () => {
-    // if(phone==="") {
-    //   setAlertMessage('Please enter your mobile number');
-    //   return;
-    // }
+    setPhone(phone);
     try {
-      const response = await fetch('http://localhost:3000/otp/phone', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("http://localhost:3000/otp/phone", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ phone }),
       });
 
       if (response.ok) {
-        setAlertMessage(`OTP sent successfully to your mobile number XXXXXXX${phone.slice(-3)}`);
+        setAlertMessage(
+          `OTP sent successfully to your mobile number XXXXXXX${phone.slice(
+            -3
+          )}`
+        );
         setShowOtp(true);
       } else {
-        setShowOtp(true);
-        setAlertMessage(`Failed to send OTP to your mobile number XXXXXXX${phone.slice(-3)}`);
+        setShowOtp(false);
+        setAlertMessage(
+          `Failed to send OTP to your mobile number XXXXXXX${phone.slice(-3)}`
+        );
       }
     } catch (error) {
-      setAlertMessage('An error occurred while sending OTP');
+      setAlertMessage("An error occurred while sending OTP");
     }
   };
 
   const handleLogin = async (currentPhone: string, currentPassword: string) => {
     try {
-      const response = await fetch('http://localhost:3000/auth/signin', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3000/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           phone: currentPhone,
           password: currentPassword,
-        })
+        }),
       });
       const data = await response.json();
       // console.log("data ", data);
 
       if (response.ok) {
-        localStorage.setItem('token', data.token);
-        setAlertMessage('Login successful');
+        localStorage.setItem("token", data.token);
+        setAlertMessage("Login successful");
       } else {
         // Handle different error responses
-        if (data.message === 'Invalid credentials') {
-          setAlertMessage('Invalid credentials');
-        } else if (data.message === 'User not found') {
-          setAlertMessage('User not found, please sign in');
+        if (data.message === "Invalid credentials") {
+          setAlertMessage("Invalid credentials");
+        } else if (data.message === "User not found") {
+          setAlertMessage("User not found, please sign in");
         } else {
-          setAlertMessage('An error occurred. Please try again.');
+          setAlertMessage("An error occurred. Please try again.");
         }
       }
     } catch (error) {
-      setAlertMessage('An error occurred. Please try again.');
-    }
-  };
-
-  const handleOtpChange = (index: number, value: string) => {
-    if (isNaN(Number(value))) return false;
-    const newOtp = [...otp];
-    newOtp[index] = value;
-    setOtp(newOtp);
-    if (value !== '') {
-      if (index < 5 && inputRefs.current[index + 1] !== null) {
-        inputRefs.current[index + 1]!.focus();
-      }
+      setAlertMessage("An error occurred. Please try again.");
     }
   };
 
@@ -116,14 +122,14 @@ const LoginForm: React.FC = () => {
     //   return;
     // }
     try {
-      const response = await fetch('http://localhost:3000/otp/verify/phone', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3000/otp/verify/phone", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           phone,
-          otp: otp.join(''),
+          otp: otp.join(""),
         }),
       });
       // console.log("response ", response);
@@ -131,25 +137,28 @@ const LoginForm: React.FC = () => {
       const data = await response.json();
       // console.log("token: ",data.token);
 
-      if (data.message === 'OTP verified successfully') {
-        localStorage.setItem('token', data.token);
-        setAlertMessage('OTP verification successful');
+      if (data.message === "OTP verified successfully") {
+        localStorage.setItem("token", data.token);
+        setAlertMessage("OTP verification successful");
       } else {
-        setAlertMessage('Invalid OTP. Please try again.');
+        setAlertMessage("Invalid OTP. Please try again.");
       }
     } catch (error) {
-      setAlertMessage('An error occurred while verifying OTP');
+      setAlertMessage("An error occurred while verifying OTP");
     }
   };
 
-  const handleKeyDown = (index: number, e: React.KeyboardEvent<HTMLInputElement>) => {
-
-    if (e.key === 'Backspace' && index > 0 && otp[index] === '' && inputRefs.current[index - 1]) {
-      inputRefs.current[index - 1]!.focus();
-    } else if (e.key === 'Enter' && !e.shiftKey && !otp.includes('')) {
-      handleOtpSubmit();
-    }
-  };
+  const phoneMask = useMaskito({
+    options: {
+      mask: [
+        ...Array(3).fill(/\d/),
+        " ",
+        ...Array(3).fill(/\d/),
+        " ",
+        ...Array(4).fill(/\d/),
+      ],
+    },
+  });
 
   return (
     <IonPage>
@@ -157,7 +166,11 @@ const LoginForm: React.FC = () => {
         <div className="form-wrapper flex justify-center items-center h-full fixed inset-0">
           <div className="w-[28rem] max-w-full shadow-lg p-6 bg-white rounded-lg">
             <h2 className="text-center text-2xl font-bold mb-4">Login</h2>
-            <IonSegment value={segment} onIonChange={handleSegmentChange} color="primary">
+            <IonSegment
+              value={segment}
+              onIonChange={handleSegmentChange}
+              color="primary"
+            >
               <IonSegmentButton value="mobile">
                 <IonLabel>Login via Mobile</IonLabel>
               </IonSegmentButton>
@@ -166,18 +179,40 @@ const LoginForm: React.FC = () => {
               </IonSegmentButton>
             </IonSegment>
 
-            {segment === 'mobile' && (
+            {segment === "mobile" && (
               <div className="mt-4">
                 <form onSubmit={handleSubmit}>
                   <IonItem>
-                    <IonInput type="tel" placeholder="Enter your mobile number" value={phone} onIonChange={(e) => setPhone(e.detail.value ?? "")} />
+                    <IonInput
+                      ref={async (phoneRef) => {
+                        if (phoneRef) {
+                          const input = await phoneRef.getInputElement();
+                          phoneMask(input);
+                        }
+                      }}
+                      type="tel"
+                      placeholder="888 888 8888"
+                      value={phone}
+                      onIonChange={(e) => setPhone(e.detail.value ?? "")}
+                      label="Mobile Number"
+                      labelPlacement="floating"
+                    />
                   </IonItem>
                   {!showPassword && !showOtp && (
                     <>
-                      <IonButton expand="full" className="mt-4" onClick={handleLoginWithPasswordClick}>
+                      <IonButton
+                        expand="full"
+                        className="mt-4"
+                        onClick={handleLoginWithPasswordClick}
+                      >
                         Login with Password
                       </IonButton>
-                      <IonButton expand="full" className="mt-2" color="medium" onClick={handleLoginWithOtpClick}>
+                      <IonButton
+                        expand="full"
+                        className="mt-2"
+                        color="medium"
+                        onClick={handleLoginWithOtpClick}
+                      >
                         Login with OTP
                       </IonButton>
                     </>
@@ -185,7 +220,12 @@ const LoginForm: React.FC = () => {
                   {showPassword && (
                     <>
                       <IonItem>
-                        <IonInput type="password" placeholder="Enter your password" value={password} onIonChange={(e) => setPassword(e.detail.value ?? "")} >
+                        <IonInput
+                          type="password"
+                          placeholder="Enter your password"
+                          value={password}
+                          onIonChange={(e) => setPassword(e.detail.value ?? "")}
+                        >
                           <IonInputPasswordToggle slot="end"></IonInputPasswordToggle>
                         </IonInput>
                       </IonItem>
@@ -196,21 +236,16 @@ const LoginForm: React.FC = () => {
                   )}
 
                   {showOtp && (
-                    <div className="otp-wrapper flex justify-between">
-                      {otp.map((_, index) => (
-                        <IonInput
-                          key={index}
-                          id={`otp-input-${index}`}
-                          className="otp-input text-center w-10 h-12 border rounded"
-                          type="tel"
-                          value={otp[index]}
-                          maxlength={1}
-                          onIonChange={(e) => handleOtpChange(index, e.detail.value ?? '')}
-                          onKeyDown={(e:any) => handleKeyDown(index, e)}
-                          ref={el => inputRefs.current[index] = el}
-                        />
-                      ))}
-                    </div>
+                    <PinInput
+                      values={otp}
+                      autoFocus={true}
+                      onChange={(value, index, values) => setOtp(values)}
+                      size="lg"
+                      containerClassName="justify-center my-4"
+                      inputClassName="mx-2"
+                      placeholder=""
+                      validBorderColor="rgb(13,110,253)"
+                    />
                   )}
 
                   {showOtp && (
@@ -222,7 +257,7 @@ const LoginForm: React.FC = () => {
               </div>
             )}
 
-            {segment === 'abha' && (
+            {segment === "abha" && (
               <>
                 <div className="mt-4">
                   <IonItem>
@@ -235,7 +270,7 @@ const LoginForm: React.FC = () => {
                 <div className="text-center mt-6">
                   <IonText color="medium">
                     <p>
-                      Don't have an ABHA ID?{' '}
+                      Don't have an ABHA ID?{" "}
                       <Link to="/create-abha" className="text-blue-500">
                         Create one
                       </Link>
@@ -252,13 +287,13 @@ const LoginForm: React.FC = () => {
           <IonAlert
             isOpen={!!alertMessage}
             onDidDismiss={() => setAlertMessage(null)}
-            header={'Alert'}
+            header={"Alert"}
             message={alertMessage}
-            buttons={['OK']}
+            buttons={["OK"]}
           />
         )}
       </IonContent>
-    </IonPage >
+    </IonPage>
   );
 };
 
